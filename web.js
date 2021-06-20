c530ee34203e5d3851bff1d17b908af82f591cdf
BasharDamen <bashar.alda@gmail.com>
2021-06-20 16:04:24 +0300

    class01

---
+++ web.js
@@ -0,0 +1,13 @@
+
+
+let userAnswer1 = prompt('Hello, What is your name?');
+let userAnswer2 = prompt('Where are you from?');
+let userAnswer3 = prompt('What type of the following do you like more: Mercedes, BMW, Tesla ?');
+let userAnswer4 = prompt('Do you have a drive license?');
+alert('You are welcom ' + userAnswer1 + " from  " + userAnswer2 + ".  " + userAnswer3 + ' is a great choice.'+ 
+' Have a nice day');
+
+console.log(userAnswer1);
+console.log(userAnswer2);
+console.log(userAnswer3);
+console.log(userAnswer3);
